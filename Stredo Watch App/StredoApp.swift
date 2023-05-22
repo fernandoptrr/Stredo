22c0b64fb5b2e7cb9afa8a1bbfaa89b630a7c59e
Fernando Putra <fernnandoptr@gmail.com>
2023-05-22 09:21:00 +0700

    Initial commit

---
+++ Stredo Watch App/StredoApp.swift
@@ -0,0 +1,17 @@
+//
+//  StredoApp.swift
+//  Stredo Watch App
+//
+//  Created by Fernando Putra on 22/05/23.
+//
+
+import SwiftUI
+
+@main
+struct Stredo_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
